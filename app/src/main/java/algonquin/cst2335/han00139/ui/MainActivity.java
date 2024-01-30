e10d3272b82b4bc95fc94630c083ea1db4a218af
Hanzg20 <hanzg2020@gmail.com>
2024-01-30 11:57:09 -0500

    Finish Step 3

--- app/src/main/java/algonquin/cst2335/han00139/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/han00139/ui/MainActivity.java
@@ -1,88 +1,40 @@
 package algonquin.cst2335.han00139.ui;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ImageButton;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
-import android.widget.RadioButton;
 import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
 import algonquin.cst2335.han00139.R;
-import algonquin.cst2335.han00139.data.MainViewModel;
-import algonquin.cst2335.han00139.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding variableBinding;
-    private MainViewModel model;
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(variableBinding.getRoot());
-        // Initialize views using ViewBinding
-        TextView mytext = variableBinding.textview;
-        Button mybutton = variableBinding.mybutton;
-        EditText myedit = variableBinding.myedittext;
+        setContentView(R.layout.activity_main);
 
-        // Set up click listener for the button
-        mybutton.setOnClickListener(view -> {
-            // Get the text from EditText
-            String editString = myedit.getText().toString();
-            // Update TextView with the text from EditText
-            mytext.setText("Your edit text has: " + editString);
+        imgView = findViewById(R.id.flagview);
+        sw = findViewById(R.id.spin_switch);
+
+        sw.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imgView.startAnimation(rotate);
+            } else {
+                imgView.clearAnimation();
+            }
         });
-
-        model = new ViewModelProvider(this).get(MainViewModel.class);
-
-        // ImageView
-        ImageView imageView = findViewById(R.id.myimageview);
-        imageView.setImageResource(R.drawable.logo_algonquin);
-
-        // ImageButton
-        ImageButton imageButton = findViewById(R.id.myimagebutton);
-        imageButton.setImageResource(R.drawable.logo_algonquin);
-        imageButton.setOnClickListener(view -> showDimensionsToast(view.getWidth(), view.getHeight()));
-
-        // Initialize your CheckBox, Switch, and RadioButton using ViewBinding
-        CheckBox checkBox = variableBinding.checkBox;
-        Switch switchButton = variableBinding.switchButton;
-        RadioButton radioButton = variableBinding.radioButton;
-
-        // Set up observers for the MutableLiveData in MainViewModel
-        model.getIsSelected().observe(this, isSelected -> {
-            checkBox.setChecked(isSelected);
-            switchButton.setChecked(isSelected);
-            radioButton.setChecked(isSelected);
-
-            // Show Toast for the state of compound buttons
-            showToastForCompoundButtons(isSelected);
-        });
-
-        // Set up OnCheckedChangeListener for the CheckBox, Switch, and RadioButton
-        checkBox.setOnCheckedChangeListener(checkedChangeListener);
-        switchButton.setOnCheckedChangeListener(checkedChangeListener);
-        radioButton.setOnCheckedChangeListener(checkedChangeListener);
-    }
-
-    private void showToastForCompoundButtons(boolean isSelected) {
-        String message = "Compound buttons state: " + (isSelected ? "Checked" : "Unchecked");
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
-    private final CompoundButton.OnCheckedChangeListener checkedChangeListener = (buttonView, isChecked) -> {
-        model.getIsSelected().setValue(isChecked);
-    };
-    private void showDimensionsToast(int width, int height) {
-        String message = "The width = " + width + " and height = " + height;
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
